395efdce92c8d30ec1b95a9315c1a6ed044673fc
Giannis Kopanas <jkopanas@gmail.com>
2026-06-24 13:02:04 +0300

    update cors to allow any local port

--- api/src/app.ts
+++ api/src/app.ts
@@ -8,7 +8,7 @@ export const store = new MemoryStore();
 
 const app = express();
 
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors({ origin: /^http:\/\/localhost(:\d+)?$/ }));
 app.use(express.json());
 app.use((req, _, next) => {
   console.log(`${req.method} ${req.path}`);
